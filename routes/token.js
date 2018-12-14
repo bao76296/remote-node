5179dbe694576733e039ed8f1aa1ce1796c1bc57
bao76296 <762963131@qq.com>
2018-12-14 22:25:27 +0800

    -m'init_3'

--- routes/token.js
+++ routes/token.js
@@ -1,9 +1,18 @@
 var express = require('express')
 var router = express.Router();
-
+var sha1 = require('sha1');
 
 router.get('/', (req, res, next) => {
-    console.log(req.query);
+    let { signature, timestamp, nonce, echostr } = req.query;
+    // token =  mywechat
+    let arr = ['mywechat', timestamp, nonce].sort().join('');
+    arr = sha1('arr');
+    if(arr=== signature){
+        res.end(echostr)
+    } else {
+        res.end('')
+    }
+
     res.send("????")
 })
 
